f7008ebbd640d72e8cd2a08baa47b9e146b26d9c
micahbaumann <micah.ad.mail@gmail.com>
2023-09-25 02:18:39 -0700

    Fixed configuration

--- api.py
+++ api.py
@@ -4,8 +4,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# This is an example endpoint. Change the "/" to the path.
-# For example: "/listclasses"
+# This is an example endpoint. Change the "/" to the path. For example: "/listclasses"
 # For each new endpoint here, create a new endpoint in the etc/krakend.json file
 @app.get("/")
 def read_root():
