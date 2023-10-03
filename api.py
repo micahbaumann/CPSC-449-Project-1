78eff8be267d65e2554302480199de4b33d09dad
Logan Langdon <logan.langdon@yahoo.com>
2023-10-02 17:19:52 -0700

    Added more endpoint functions and populated database with more tables and entries including instructors

--- api.py
+++ api.py
@@ -10,9 +10,12 @@ class Settings(BaseSettings, env_file=".env", extra="ignore"):
     database: str
     logging_config: str
 
-class Course(BaseModel):
+class Frozen(BaseSettings, env_file=".env", extra="ignore"):
+    is_frozen: bool
+
+class Classes(BaseModel):
     Department: str
-    course_code: int
+    class_code: str
     section_number: int
     name: str
     Instructor: int
@@ -25,17 +28,56 @@ def get_db():
         yield db
 
 settings = Settings()
+frozen = Frozen()
 app = FastAPI()
 
 # This is an example endpoint. Change the "/" to the path. For example: "/listclasses"
 # For each new endpoint here, create a new endpoint in the etc/krakend.json file
 
+### Student related endpoints
 @app.get("/")
 def greet():
     return {"Hello": "World"}
 
 @app.get("/list/")
 def list_open_classes(db: sqlite3.Connection = Depends(get_db)):
-    classes = db.execute("SELECT * FROM Course")
+    classes = db.execute("SELECT * FROM Classes WHERE Classes.CurrentEnrollment < Classes.MaximumEnrollment")
+    return {"Classes": classes.fetchall()}
 
-    return {"Courses": classes.fetchall()}
+@app.post("/enroll/{studentid}/{classid}")
+def enroll_student_in_class(studentid: int, classid: str, db: sqlite3.Connection = Depends(get_db)):
+    return {}
+
+@app.delete("/drop/{studentid}/{classid}")
+def drop_student_from_class(studentid: int, classid: str, db: sqlite3.Connection = Depends(get_db)):
+    return {}
+
+@app.get("/waitlist/{studentid}/{classid}")
+def view_waitlist_position(studentid: int, classid: str, db: sqlite3.Connection = Depends(get_db)):
+    return {}
+
+
+### Instructor related endpoints
+
+@app.get("enrolled/{instructorid}/{classid}")
+def view_enrolled(instructorid: int, classid: str, db: sqlite3.Connection = Depends(get_db)):
+    return {}
+
+@app.get("dropped/{instructorid}/{classid}")
+def view_dropped(instructorid: int, classid: str, db: sqlite3.Connection = Depends(get_db)):
+    return {}
+
+@app.delete("drop/{instructorid}/{classid}/{studentid}")
+def drop_student(instructorid: int, classid: str,studentid,int, db: sqlite3.Connection = Depends(get_db)):
+    return {}
+
+
+
+
+
+### Registrar related endpoints
+
+@app.put("/freeze/")
+def view_waitlist_position():
+    frozen.is_frozen = True
+    return {}
---
+++ var/catalog.sql
@@ -0,0 +1,55 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+
+CREATE TABLE Classes (
+    CourseCode VARCHAR(5) PRIMARY KEY NOT NULL UNIQUE,
+    SectionNumber INT SECONDARY KEY NOT NULL,
+    Name VARCHAR(100),
+    InstructorID INT,
+    CurrentEnrollment INT,
+    MaximumEnrollment INT
+);
+
+CREATE TABLE Students (
+    StudentID VARCHAR(5) PRIMARY KEY NOT NULL UNIQUE
+);
+
+CREATE TABLE StudentClasses (
+    StudentID VARCHAR(5) PRIMARY KEY NOT NULL,
+    ClassID VARCHAR(5),
+    SectionID VARCHAR(5),
+    EnrollmentStatus INT
+);
+
+CREATE TABLE Instructors (
+    InstructorID VARCHAR(5) PRIMARY KEY NOT NULL UNIQUE
+);
+
+CREATE TABLE InstructorClasses (
+    InstructorID VARCHAR(5) PRIMARY KEY NOT NULL,
+    ClassID VARCHAR(5),
+    SectionID VARCHAR(5)
+);
+
+CREATE TABLE Freeze (
+    IsFrozen BOOLEAN DEFAULT 0
+);
+
+INSERT INTO Classes VALUES
+("120A",5,'Introduction to Programming',1,15,30),
+("121",5,'Object-Oriented Programming',1,30,30),
+("131",5,'Data Structures',1,25,30);
+
+INSERT INTO Students VALUES ("001"),("002"),("003"), ("004"),("005"),("006"),("007"),("008"),("009"),("010");
+
+INSERT INTO StudentClasses VALUES
+("001", "120A", 5, 0),
+("002", "121",  5, 0);
+
+
+INSERT INTO Instructors VALUES ("001"),("002"),("003"),("004"),("005");
+
+INSERT INTO InstructorClasses VALUES 
+("001","120A",5);
+
+COMMIT;
